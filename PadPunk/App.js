7755cd8fa15a3e67728bf50287903ae197137781
JeromeHdz <hernandez.jrme@gmail.com>
2019-06-03 18:59:22 +0900

    new BG + audio (low high normal)

--- PadPunk/App.js
+++ PadPunk/App.js
@@ -1,5 +1,7 @@
 let lowPitch = false;
+let pitch = "normal";
 let root = document.documentElement;
+let toggleKey = ""; 
 
 function playSound(e){    
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -20,16 +22,17 @@ function playSound(e){
             }
         break;
         case 188:
-                if(lowPitch == false){
-                    root.style.setProperty('--main-color', 'rgb(212, 0, 255)');
-                    lowPitch = true;
-                }
-                else{
-                    lowPitch = false;
-                    root.style.setProperty('--main-color', "rgb(0, 217, 255)");
-                    key.classList.remove('playing');
-                }
-            break;
+            pitch = "normal";
+            switchPitch();
+         break;
+        case 190:
+            pitch = "low";
+            switchPitch();
+        break;
+        case 191:
+            pitch = "high";
+            switchPitch();
+        break;
         default:
           // code block
           audio.currentTime = 0;
@@ -41,6 +44,38 @@ function playSound(e){
   function removeTransition(e){
     if(e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
+  }
+
+  function switchPitch(){
+      
+      switch(pitch) {
+            case pitch = "low":
+               toggleKey = Array.from(document.querySelectorAll('.toggle:not(.low)'));
+               toggleKey.forEach(key => key.classList.remove('playing'));
+               root.style.setProperty('--main-color', 'rgb(255, 0, 149)');
+               let audioSrc = Array.from(document.querySelectorAll('.has-low'));
+               audioSrc.forEach(srcA => srcA.src = srcA.src.replace(/-.*\./, '-low.'));
+            break;
+            
+            case pitch = "high":
+                    toggleKey = Array.from(document.querySelectorAll('.toggle:not(.high)'));
+                    toggleKey.forEach(key => key.classList.remove('playing'));
+                root.style.setProperty('--main-color', "rgb(0, 255, 115)"); 
+                let audioSrch = Array.from(document.querySelectorAll('audio'));
+                audioSrch.forEach(srcA => srcA.src = srcA.src.replace(/-.*\./, '-high.'));  
+                console.log(audioSrch);
+            break;
+
+            default:
+                    toggleKey = Array.from(document.querySelectorAll('.toggle:not(.normal)'));
+                    toggleKey.forEach(key => key.classList.remove('playing'));
+                    root.style.setProperty('--main-color', "rgb(0, 217, 255)"); 
+                    let audioSrcn = Array.from(document.querySelectorAll('audio'));
+                    audioSrcn.forEach(srcA => srcA.src = srcA.src.replace(/-.*\./, '-normal.')); 
+                    
+                    
+  
+      }
   }
 
  
